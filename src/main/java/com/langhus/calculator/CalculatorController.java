29ce0a3062f53f8f1a7f984fe05d190a76a8ada2
langhus <langhus2020@outlook.com>
2020-03-12 14:25:29 -0700

    added calculator

--- src/main/java/com/langhus/calculator/CalculatorController.java
+++ src/main/java/com/langhus/calculator/CalculatorController.java
@@ -1,4 +1,5 @@
-import com.langhus.Calculator;
+//import com.langhus.Calculator;
+import Calculator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
